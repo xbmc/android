ee9e5e34aa92b25c293029e014a05dce33a4ee55
Phaeodaria <phaeodaria@xbmc.org>
2011-06-12 13:29:07 +0200

    TCPServer.h - add missing types for android

--- xbmc/network/TCPServer.h
+++ xbmc/network/TCPServer.h
@@ -7,6 +7,37 @@
 #include "threads/CriticalSection.h"
 #include "interfaces/json-rpc/JSONUtils.h"
 
+#if defined(__ANDROID__)
+// android does not defined sockaddr_storage, this is the definition from RFC2553
+
+#define _SS_MAXSIZE    128  /* Implementation specific max size */
+#define _SS_ALIGNSIZE  (sizeof (int64_t))
+                         /* Implementation specific desired alignment */
+/*
+ * Definitions used for sockaddr_storage structure paddings design.
+ */
+typedef __sa_family_t sa_family_t;
+typedef __socklen_t socklen_t;
+
+#define _SS_PAD1SIZE   (_SS_ALIGNSIZE - sizeof (sa_family_t))
+#define _SS_PAD2SIZE   (_SS_MAXSIZE - (sizeof (sa_family_t)+_SS_PAD1SIZE + _SS_ALIGNSIZE))
+struct sockaddr_storage {
+    sa_family_t  __ss_family;     /* address family */
+    /* Following fields are implementation specific */
+    char      __ss_pad1[_SS_PAD1SIZE];
+              /* 6 byte pad, this is to make implementation
+              /* specific pad up to alignment field that */
+              /* follows explicit in the data structure */
+    int64_t   __ss_align;     /* field to force desired structure */
+               /* storage alignment */
+    char      __ss_pad2[_SS_PAD2SIZE];
+              /* 112 byte pad to achieve desired size, */
+              /* _SS_MAXSIZE value minus size of ss_family */
+              /* __ss_pad1, __ss_align fields is 112 */
+};
+
+#endif
+
 class CVariant;
 namespace JSONRPC
 {
