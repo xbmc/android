2ea68eacb8b267377841e19833a3a13edadd98a8
davilla <davilla@4pi.com>
2012-02-26 20:02:14 -0500

    [droid] android make depends tweeks

--- tools/android/depends/android-sdk.mk
+++ tools/android/depends/android-sdk.mk
@@ -1,19 +1,20 @@
 export ARCH_FLAGS=-march=armv7-a -mtune=cortex-a9 -mfloat-abi=softfp -mfpu=neon
 export HOST=arm-linux-androideabi
+export SYSROOT=$(TOOLCHAIN)/sysroot
 export CROSSTOOLS=$(TOOLCHAIN)/bin/arm-linux-androideabi-
+export LD=$(CROSSTOOLS)ld
 export CC=$(CROSSTOOLS)gcc
 export CXX=$(CROSSTOOLS)g++
 export AR=$(CROSSTOOLS)ar
-export LD=$(CROSSTOOLS)ld
 export AS=$(CROSSTOOLS)as
 export NM=$(CROSSTOOLS)nm
 export STRIP=$(CROSSTOOLS)strip
 export RANLIB=$(CROSSTOOLS)ranlib
 export OBJDUMP=$(CROSSTOOLS)objdump
-export CFLAGS=$(ARCH_FLAGS) -isystem$(XBMCPREFIX)/include
+export CFLAGS=$(ARCH_FLAGS) -I$(XBMCPREFIX)/include
+export LDFLAGS=$(ARCH_FLAGS) -L$(XBMCPREFIX)/lib
 export CPPFLAGS=$(CFLAGS)
 export CXXFLAGS=$(CFLAGS)
-export LDFLAGS=$(ARCH_FLAGS) -L$(XBMCPREFIX)/lib
 export LIBS=-lstdc++
 export PKG_CONFIG_PATH=$(XBMCPREFIX)/lib/pkgconfig
 export PATH:=${XBMCPREFIX}/bin:${TOOLCHAIN}/bin:$(PATH)
