8ac02202ce29855e9f42431a7ce470e44274f0c9
theuni <theuni-nospam-@xbmc.org>
2012-03-01 15:43:20 -0500

    [droid] add rudimentary but working packager

--- tools/android/packaging/xbmc/jni/lib/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := xbmc
-LOCAL_SRC_FILES := libxbmc.so
-include $(PREBUILT_SHARED_LIBRARY)
--- tools/android/packaging/xbmc/jni/lib/include/xbmc.h
+++
@@ -1,4 +0,0 @@
-#include <stdbool.h>
-#include <stdint.h>
-
-bool XBMC_Init(unsigned long, const char*, int, const char**);
