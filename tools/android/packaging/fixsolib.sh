cf8577e8116dd9498e5813c90a67febe959e202c
montellese <montellese@xbmc.org>
2012-03-07 11:07:07 +0100

    [droid] fix loading of libcpluff-arm.so
    Loading failed because libcpluff depends on libexpat.so.1 by default
    which needs to be fixed to libexpat.so

--- tools/android/packaging/fixsolib.sh
+++ tools/android/packaging/fixsolib.sh
@@ -10,7 +10,7 @@ rpl -R -e libjpeg.so.8 "libjpeg.so\x00\x00" xbmc/libs/armeabi/
 rpl -R -e libgcrypt.so.11 "libgcrypt.so\x00\x00\x00" xbmc/libs/armeabi/
 rpl -R -e libgpg-error.so.0 "libgpg-error.so\x00\x00" xbmc/libs/armeabi/
 rpl -R -e libfontconfig.so.1 "libfontconfig.so\x00\x00" xbmc/libs/armeabi/
-rpl -R -e libexpat.so.1 "libexpat.so\x00\x00" xbmc/libs/armeabi/
+rpl -R -e libexpat.so.1 "libexpat.so\x00\x00" xbmc/libs/armeabi/ xbmc/assets/system/
 rpl -R -e libfreetype.so.6 "libfreetype.so\x00\x00" xbmc/libs/armeabi/
 rpl -R -e libiconv.so.2 "libiconv.so\x00\x00" xbmc/libs/armeabi/
 rpl -R -e libfribidi.so.0 "libfribidi.so\x00\x00" xbmc/libs/armeabi/
