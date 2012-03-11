5d482f0c74287888ee6f52f052e8f7f4af1a0dd1
montellese <montellese@xbmc.org>
2012-03-11 06:48:13 -0700

    [droid] introduce CPlatformUtils to provide (decoupled) access to the XBMC_PLATFORM instance
    Conflicts:
    
    	xbmc/Application.cpp
    	xbmc/Application.h
    	xbmc/utils/log.cpp
    	xbmc/windowing/egl/WinEGLPlatformAndroid.cpp

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -312,6 +312,8 @@
 #include "XHandle.h"
 #endif
 
+#include "utils/PlatformUtils.h"
+
 #ifdef HAS_LIRC
 #include "input/linux/LIRC.h"
 #endif
@@ -1121,6 +1123,15 @@ void CApplication::CreateUserDirs()
   CDirectory::Create("special://masterprofile/");
   CDirectory::Create("special://temp/");
   CDirectory::Create("special://temp/temp"); // temp directory for python and dllGetTempPathA
+}
+
+void CApplication::PlatformInitialize(XBMC_PLATFORM *platform)
+{
+  if (platform)
+  {
+    m_sLogName = platform->log_name;
+    CPlatformUtils::SetPlatform(platform);
+  }
 }
 
 bool CApplication::Initialize()
