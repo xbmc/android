784364562e1070312907d0e99af769ab24102d62
theuni <theuni-nospam-@xbmc.org>
2012-04-25 15:41:45 -0400

    [droid] beginnings of a wrapper for dlopen.
    Bionic's dlopen does not load libs recursively, so we have to do it ourselves.
    This means peeking into the elf to find the DT_NEEDED libs and loading them
    first. Additionally we must be wary of circular depends, so a simple recursive
    open does not suffice.
    
    For now this is borrowed from libreoffice's implementation. We'll need to
    rewrite it before release as theirs is gplv3.
    
    These will need to be moved into some class, but don't bother now until we know
    what other utils we might need.
    
    android_utils are built as a static lib so that they can be shared between
    libxbmc and the stub app.

--- xbmc/android/android_main.cpp
+++ xbmc/android/android_main.cpp
@@ -7,8 +7,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <dlfcn.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <linux/elf.h>
+#include <fcntl.h>
+#include <errno.h>
 #include "xbmc.h"
+#include "android_utils.h"
 
 typedef struct {
   struct android_app* app;
@@ -26,43 +32,6 @@ static int android_printf(const char *format, ...)
   int result = __android_log_vprint(ANDROID_LOG_VERBOSE, "XBMC", format, args);
   va_end(args);
   return result;
-}
-
-void* tryopen(const char *path)
-{
-  void *handle;
-  handle = dlopen(path, RTLD_NOW);
-  if (!handle)
-  {
-    __android_log_print(ANDROID_LOG_VERBOSE, "XBMC", "could not load %s. Error: %s",path, dlerror());
-    exit(1);
-  }
-  __android_log_print(ANDROID_LOG_VERBOSE, "XBMC", "loaded lib: %s",path);
-  return handle;
-}
-
-void preload()
-{
-  tryopen("/data/data/org.xbmc/lib/libyajl.so");
-  tryopen("/data/data/org.xbmc/lib/libtiff.so");
-  tryopen("/data/data/org.xbmc/lib/libxbjpeg.so");
-  tryopen("/data/data/org.xbmc/lib/libgpg-error.so");
-  tryopen("/data/data/org.xbmc/lib/libgcrypt.so");
-  tryopen("/data/data/org.xbmc/lib/libiconv.so");
-  tryopen("/data/data/org.xbmc/lib/libfreetype.so");
-  tryopen("/data/data/org.xbmc/lib/libfontconfig.so");
-  tryopen("/data/data/org.xbmc/lib/libfribidi.so");
-  tryopen("/data/data/org.xbmc/lib/libsqlite3.so");
-  tryopen("/data/data/org.xbmc/lib/libxbpng.so");
-  tryopen("/data/data/org.xbmc/lib/libpcre.so");
-  tryopen("/data/data/org.xbmc/lib/libsamplerate.so");
-  tryopen("/data/data/org.xbmc/lib/libpython2.6.so");
-  tryopen("/data/data/org.xbmc/lib/libpcrecpp.so");
-  tryopen("/data/data/org.xbmc/lib/libzip.so");
-
-  // gdb sleeps before attaching for some reason. let it attach before we try
-  // to load libxbmc.
-  sleep(1);
 }
 
 void setup_env(struct android_app* state)
@@ -247,11 +216,9 @@ extern void android_main(struct android_app* state)
   // make sure that the linker doesn't strip out our glue
   app_dummy();
 
-  preload();
   setup_env(state);
-
   void* soHandle;
-  soHandle = tryopen("/data/data/org.xbmc/lib/libxbmc.so");
+  soHandle = lo_dlopen("/data/data/org.xbmc/lib/libxbmc.so");
 
   // Setting up the context
   android_context context;
