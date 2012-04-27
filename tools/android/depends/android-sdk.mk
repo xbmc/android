7527ed717ac346a53af21570da192bb5f167dfe7
theuni <theuni-nospam-@xbmc.org>
2012-04-27 17:44:38 -0400

    [droid] fixups for crystax gcc 4.6
    Crystax does not enable rtti or exceptions by default. Enable them here.
    Shouldn't affect ndk builds where they're already enabled anyway.
    
    Also switch to armv7 by default. Uncomment the others for v7+neon or
    emulator usage.

--- tools/android/depends/android-sdk.mk
+++ tools/android/depends/android-sdk.mk
@@ -1,5 +1,6 @@
-#export ARCH_FLAGS=-march=armv7-a -mtune=cortex-a9 -mfloat-abi=softfp -mfpu=neon
-export ARCH_FLAGS=-march=armv5te -mtune=xscale -msoft-float -D__ARM_ARCH_5__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5TE__ -DANDROID
+#export ARCH_FLAGS=-march=armv7-a -mtune=cortex-a9 -mfloat-abi=softfp -mfpu=neon -D__ARM_ARCH_7__ -D__ARM_ARCH_7A__ -DANDROID
+export ARCH_FLAGS=-march=armv7-a -mtune=cortex-a9 -mfloat-abi=softfp -D__ARM_ARCH_7__ -D__ARM_ARCH_7A__ -DANDROID
+#export ARCH_FLAGS=-march=armv5te -mtune=xscale -msoft-float -D__ARM_ARCH_5__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5TE__ -DANDROID
 export HOST=arm-linux-androideabi
 export SYSROOT=$(TOOLCHAIN)/sysroot
 export CROSSTOOLS=$(TOOLCHAIN)/bin/arm-linux-androideabi-
@@ -12,10 +13,10 @@ export NM=$(CROSSTOOLS)nm
 export STRIP=$(CROSSTOOLS)strip
 export RANLIB=$(CROSSTOOLS)ranlib
 export OBJDUMP=$(CROSSTOOLS)objdump
-export CFLAGS=$(ARCH_FLAGS) -I$(XBMCPREFIX)/include
+export CFLAGS=$(ARCH_FLAGS) -I$(XBMCPREFIX)/include -fexceptions
 export LDFLAGS=$(ARCH_FLAGS) -L$(XBMCPREFIX)/lib
-export CPPFLAGS=$(CFLAGS)
-export CXXFLAGS=$(CFLAGS)
+export CPPFLAGS=$(ARCH_FLAGS) -I$(XBMCPREFIX)/include -fexceptions -frtti
+export CXXFLAGS=$(ARCH_FLAGS) -I$(XBMCPREFIX)/include -fexceptions -frtti
 export LIBS=-lstdc++
 export PKG_CONFIG_PATH=$(XBMCPREFIX)/lib/pkgconfig
 export PATH:=${XBMCPREFIX}/bin:${TOOLCHAIN}/bin:$(PATH)
