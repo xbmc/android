ba811f9cbb4c97b0baeb9e844ada6e6749f8f680
montellese <montellese@xbmc.org>
2012-04-28 13:24:27 +0200

    [droid] build with NEON support by default

--- tools/android/depends/android-sdk.mk
+++ tools/android/depends/android-sdk.mk
@@ -1,5 +1,5 @@
-#export ARCH_FLAGS=-march=armv7-a -mtune=cortex-a9 -mfloat-abi=softfp -mfpu=neon -D__ARM_ARCH_7__ -D__ARM_ARCH_7A__ -DANDROID
-export ARCH_FLAGS=-march=armv7-a -mtune=cortex-a9 -mfloat-abi=softfp -D__ARM_ARCH_7__ -D__ARM_ARCH_7A__ -DANDROID
+export ARCH_FLAGS=-march=armv7-a -mtune=cortex-a9 -mfloat-abi=softfp -mfpu=neon -D__ARM_ARCH_7__ -D__ARM_ARCH_7A__ -DANDROID
+#export ARCH_FLAGS=-march=armv7-a -mtune=cortex-a9 -mfloat-abi=softfp -D__ARM_ARCH_7__ -D__ARM_ARCH_7A__ -DANDROID
 #export ARCH_FLAGS=-march=armv5te -mtune=xscale -msoft-float -D__ARM_ARCH_5__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5TE__ -DANDROID
 export HOST=arm-linux-androideabi
 export SYSROOT=$(TOOLCHAIN)/sysroot
