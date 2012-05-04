6b8cbca9b9ac58de74df22213abc190e1381001e
theuni <theuni-nospam-@xbmc.org>
2012-05-04 00:01:03 -0400

    [droid] beginning of platform separation
    Split builds out by platform. This will allow us to build for armv7/armv6/x86
    etc and package it all together.

--- tools/android/depends/android-sdk.mk
+++ tools/android/depends/android-sdk.mk
@@ -1,9 +1,5 @@
-export ARCH_FLAGS=-march=armv7-a -mtune=cortex-a9 -mfloat-abi=softfp -mfpu=neon -D__ARM_ARCH_7__ -D__ARM_ARCH_7A__ -DANDROID -Os
-#export ARCH_FLAGS=-march=armv7-a -mtune=cortex-a9 -mfloat-abi=softfp -D__ARM_ARCH_7__ -D__ARM_ARCH_7A__ -DANDROID -Os
-#export ARCH_FLAGS=-march=armv5te -mtune=xscale -msoft-float -D__ARM_ARCH_5__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5TE__ -DANDROID -Os
-export HOST=arm-linux-androideabi
 export SYSROOT=$(TOOLCHAIN)/sysroot
-export CROSSTOOLS=$(TOOLCHAIN)/bin/arm-linux-androideabi-
+export CROSSTOOLS=$(TOOLCHAIN)/bin/$(HOST)-
 export LD=$(CROSSTOOLS)ld
 export CC=$(CROSSTOOLS)gcc
 export CXX=$(CROSSTOOLS)g++
@@ -13,11 +9,10 @@ export NM=$(CROSSTOOLS)nm
 export STRIP=$(CROSSTOOLS)strip
 export RANLIB=$(CROSSTOOLS)ranlib
 export OBJDUMP=$(CROSSTOOLS)objdump
-export CFLAGS=$(ARCH_FLAGS) -I$(XBMCPREFIX)/include -fexceptions
-export LDFLAGS=$(ARCH_FLAGS) -L$(XBMCPREFIX)/lib
-export CPPFLAGS=$(ARCH_FLAGS) -I$(XBMCPREFIX)/include -fexceptions -frtti
-export CXXFLAGS=$(ARCH_FLAGS) -I$(XBMCPREFIX)/include -fexceptions -frtti
+export CFLAGS=$(PLATFORM_FLAGS) -I$(PREFIX)/include -fexceptions
+export LDFLAGS=$(PLATFORM_FLAGS) -L$(PREFIX)/lib
+export CPPFLAGS=$(PLATFORM_FLAGS) -I$(PREFIX)/include -fexceptions -frtti
+export CXXFLAGS=$(PLATFORM_FLAGS) -I$(PREFIX)/include -fexceptions -frtti
 export LIBS=-lstdc++
-export PKG_CONFIG_PATH=$(XBMCPREFIX)/lib/pkgconfig
-export PATH:=${XBMCPREFIX}/bin:${TOOLCHAIN}/bin:$(PATH)
-export PREFIX=$(XBMCPREFIX)
+export PKG_CONFIG_PATH=$(PREFIX)/lib/pkgconfig
+export PATH:=${PREFIX}/bin:${TOOLCHAIN}/bin:$(PATH)
--- tools/android/depends/setup-sdk.sh
+++ tools/android/depends/setup-sdk.sh
@@ -3,28 +3,35 @@
 SCRIPT_PATH=$(cd `dirname $0` && pwd)
 
 #Edit these
-NDKROOT=
-SDKROOT=
-TARBALLS=
-TOOLCHAIN=
-XBMCPREFIX=
+NDKROOT=/opt/android/aml/android-ndk-r7-crystax-5.beta2
+SDKROOT=/opt/android/aml/android-sdk-linux
+TARBALLS_LOCATION=/opt/xbmc-tarballs
+TOOLCHAIN=/opt/android/aml/android-ndk-r7-crystax-5.beta2-toolchain
+XBMCPREFIX=/opt/xbmc-deps
+HOST=arm-linux-androideabi
+PLATFORM=armeabi-v7a
+PLATFORM_FLAGS="-march=armv7-a -mtune=cortex-a9 -mfloat-abi=softfp -mfpu=neon -D__ARM_ARCH_7__ -D__ARM_ARCH_7A__ -DANDROID -Os"
 
 #
 #
-mkdir -p $XBMCPREFIX $TARBALLS
-chown -R $USER:$USER $XBMCPREFIX $TARBALLS
-mkdir -p $XBMCPREFIX/lib $XBMCPREFIX/include
+mkdir -p $XBMCPREFIX/$PLATFORM $TARBALLS_LOCATION
+sudo chown -R $USER:$USER $XBMCPREFIX/$PLATFORM $TARBALLS_LOCATION
+mkdir -p $XBMCPREFIX/$PLATFORM/lib $XBMCPREFIX/$PLATFORM/include
 
 #
 #
 echo "NDKROOT=$NDKROOT"                                                >  $SCRIPT_PATH/Makefile.include
 echo "SDKROOT=$SDKROOT"                                                >> $SCRIPT_PATH/Makefile.include
 echo "XBMCPREFIX=$XBMCPREFIX"                                          >> $SCRIPT_PATH/Makefile.include
+echo "HOST=$HOST"                                                      >> $SCRIPT_PATH/Makefile.include
+echo "PLATFORM=$PLATFORM"                                              >> $SCRIPT_PATH/Makefile.include
+echo "PREFIX=$XBMCPREFIX/\$(PLATFORM)"                                 >> $SCRIPT_PATH/Makefile.include
+echo "PLATFORM_FLAGS=$PLATFORM_FLAGS"                                  >> $SCRIPT_PATH/Makefile.include
 echo "TOOLCHAIN=$TOOLCHAIN"                                            >> $SCRIPT_PATH/Makefile.include
 echo "BASE_URL=http://mirrors.xbmc.org/build-deps/darwin-libs"         >> $SCRIPT_PATH/Makefile.include
-echo "TARBALLS_LOCATION=$TARBALLS"                                     >> $SCRIPT_PATH/Makefile.include
+echo "TARBALLS_LOCATION=$TARBALLS_LOCATION"                            >> $SCRIPT_PATH/Makefile.include
 echo "RETRIEVE_TOOL=/usr/bin/curl"                                     >> $SCRIPT_PATH/Makefile.include
-echo "RETRIEVE_TOOL_FLAGS=-Ls --create-dirs --output \$(TARBALLS_LOCATION)/\$(ARCHIVE)" >> $SCRIPT_PATH/Makefile.include
+echo "RETRIEVE_TOOL_FLAGS=-Ls --create-dirs -f --output \$(TARBALLS_LOCATION)/\$(ARCHIVE)" >> $SCRIPT_PATH/Makefile.include
 echo "ARCHIVE_TOOL=/bin/tar"                                           >> $SCRIPT_PATH/Makefile.include
-echo "ARCHIVE_TOOL_FLAGS=xf"                                           >> $SCRIPT_PATH/Makefile.include
+echo "ARCHIVE_TOOL_FLAGS=-C \$(PLATFORM) --strip-components=1 -xf"     >> $SCRIPT_PATH/Makefile.include
 echo "JOBS=$((`grep -c processor /proc/cpuinfo` -1))"                  >> $SCRIPT_PATH/Makefile.include
