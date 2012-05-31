88cb9b3fa594d7d14bb0bbe2b3acec2fc67fcf81
theuni <theuni-nospam-@xbmc.org>
2012-05-31 14:49:47 -0400

    [droid] add apk handler for vfs

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -654,6 +654,7 @@ bool CFileItem::IsFileFolder() const
   return (
     IsSmartPlayList() ||
    (IsPlayList() && g_advancedSettings.m_playlistAsFolders) ||
+    IsAPK() ||
     IsZIP() ||
     IsRAR() ||
     IsRSS() ||
@@ -744,6 +745,11 @@ bool CFileItem::IsBDFile() const
 bool CFileItem::IsRAR() const
 {
   return URIUtils::IsRAR(m_strPath);
+}
+
+bool CFileItem::IsAPK() const
+{
+  return URIUtils::IsAPK(m_strPath);
 }
 
 bool CFileItem::IsZIP() const
