d75e4359bed2c7371059efce50046a0002189b15
theuni <theuni-nospam-@xbmc.org>
2012-06-02 01:14:43 -0400

    [droid] add libxbmc interface in anticipation of shared-lib merge

--- xbmc/xbmc.cpp
+++ xbmc/xbmc.cpp
@@ -27,70 +27,38 @@
 //   - xbfilezilla : doesnt support section loading yet
 //
 
-#include "system.h"
-#include "settings/AppParamParser.h"
-#include "settings/AdvancedSettings.h"
-#include "FileItem.h"
 #include "Application.h"
-#include "PlayListPlayer.h"
 #include "utils/log.h"
-#ifdef _LINUX
-#include <sys/resource.h>
-#include <signal.h>
-#endif
-#ifdef __APPLE__
-#include "Util.h"
-#endif
-#ifdef HAS_LIRC
-#include "input/linux/LIRC.h"
-#endif
+#include "xbmc.h"
+#include "windowing/WinEvents.h"
+#include "guilib/GUIWindowManager.h"
 
-int main(int argc, char* argv[])
+int XBMC_Run(bool renderGUI, XBMC_PLATFORM *platform)
 {
-  int status = -1;
-  //this can't be set from CAdvancedSettings::Initialize() because it will overwrite
-  //the loglevel set with the --debug flag
-#ifdef _DEBUG
-  g_advancedSettings.m_logLevel     = LOG_LEVEL_DEBUG;
-  g_advancedSettings.m_logLevelHint = LOG_LEVEL_DEBUG;
-#else
-  g_advancedSettings.m_logLevel     = LOG_LEVEL_NORMAL;
-  g_advancedSettings.m_logLevelHint = LOG_LEVEL_NORMAL;
-#endif
-  CLog::SetLogLevel(g_advancedSettings.m_logLevel);
+  int status = 0;
+  g_application.PlatformInitialize(platform);
 
-#ifdef _LINUX
-#if defined(DEBUG)
-  struct rlimit rlim;
-  rlim.rlim_cur = rlim.rlim_max = RLIM_INFINITY;
-  if (setrlimit(RLIMIT_CORE, &rlim) == -1)
-    CLog::Log(LOGDEBUG, "Failed to set core size limit (%s)", strerror(errno));
-#endif
-  // Prevent child processes from becoming zombies on exit if not waited upon. See also Util::Command
-  struct sigaction sa;
-  memset(&sa, 0, sizeof(sa));
-
-  sa.sa_flags = SA_NOCLDWAIT;
-  sa.sa_handler = SIG_IGN;
-  sigaction(SIGCHLD, &sa, NULL);
-#endif
-  setlocale(LC_NUMERIC, "C");
-  g_advancedSettings.Initialize();
-  
-#ifndef _WIN32
-  CAppParamParser appParamParser;
-  appParamParser.Parse((const char **)argv, argc);
-#endif
-  g_application.Preflight();
   if (!g_application.Create())
   {
     fprintf(stderr, "ERROR: Unable to create application. Exiting\n");
     return status;
   }
 
+  if (renderGUI && !g_application.CreateGUI())
+  {
+    fprintf(stderr, "ERROR: Unable to create GUI. Exiting\n");
+    return status;
+  }
+
+  if (!g_application.Initialize())
+  {
+    fprintf(stderr, "ERROR: Unable to Initialize. Exiting\n");
+    return status;
+  }
+
   try
   {
-    status = g_application.Run();
+    status = g_application.Run(renderGUI);
   }
   catch(...)
   {
@@ -99,4 +67,86 @@ int main(int argc, char* argv[])
   }
 
   return status;
+}
+
+extern "C" void XBMC_Pause(bool pause)
+{
+  CLog::Log(LOGDEBUG, "XBMC_Pause(%s)", pause ? "true" : "false");
+  // Only send the PAUSE action if we are pausing XBMC and something is currently playing
+  if (pause && g_application.IsPlaying() && !g_application.IsPaused())
+    g_application.getApplicationMessenger().SendAction(CAction(ACTION_PAUSE), WINDOW_INVALID, true);
+  
+  g_application.m_AppActive = g_application.m_AppFocused = !pause;
+}
+
+extern "C" void XBMC_Stop()
+{
+  g_application.getApplicationMessenger().Quit();
+}
+
+extern "C" void XBMC_Key(uint8_t code, uint16_t key, uint16_t modifiers, bool up)
+{
+  XBMC_Event newEvent;
+  memset(&newEvent, 0, sizeof(newEvent));
+
+  unsigned char type = up ? XBMC_KEYUP : XBMC_KEYDOWN;
+  newEvent.type = type;
+  newEvent.key.type = type;
+  newEvent.key.keysym.scancode = code;
+  newEvent.key.keysym.sym = (XBMCKey)key;
+  newEvent.key.keysym.unicode = key;
+  newEvent.key.keysym.mod = (XBMCMod)modifiers;
+
+  CLog::Log(LOGDEBUG, "XBMC_Key(%u, %u, 0x%04X, %d)", code, key, modifiers, up);
+  CWinEvents::MessagePush(&newEvent);
+}
+
+extern "C" void XBMC_Touch(uint8_t type, uint8_t button, uint16_t x, uint16_t y)
+{
+  XBMC_Event newEvent;
+  memset(&newEvent, 0, sizeof(newEvent));
+
+  newEvent.type = type;
+  newEvent.button.type = type;
+  newEvent.button.button = button;
+  newEvent.button.x = x;
+  newEvent.button.y = y;
+
+  CLog::Log(LOGDEBUG, "XBMC_Touch(%u, %u, %u, %u)", type, button, x, y);
+  CWinEvents::MessagePush(&newEvent);
+}
+
+extern "C" void XBMC_TouchGesture(int32_t action, float posX, float posY, float offsetX, float offsetY)
+{
+  CLog::Log(LOGDEBUG, "XBMC_TouchGesture(%d, %f, %f, %f, %f)", action, posX, posY, offsetX, offsetY);
+  if (action == ACTION_GESTURE_BEGIN)
+    g_application.getApplicationMessenger().SendAction(CAction(action, 0, posX, posY, 0, 0), WINDOW_INVALID, false);
+  else if (action == ACTION_GESTURE_PAN)
+    g_application.getApplicationMessenger().SendAction(CAction(action, 0, posX, posY, offsetX, offsetY), WINDOW_INVALID, false);
+  else if (action == ACTION_GESTURE_END)
+    g_application.getApplicationMessenger().SendAction(CAction(action, 0, posX, posY, offsetX, offsetY), WINDOW_INVALID, false);
+  else if (action == ACTION_GESTURE_ZOOM)
+    g_application.getApplicationMessenger().SendAction(CAction(action, 0, posX, posY, offsetX, 0), WINDOW_INVALID, false);
+}
+
+extern "C" int XBMC_TouchGestureCheck(float posX, float posY)
+{
+  CLog::Log(LOGDEBUG, "XBMC_TouchGestureCheck(%f, %f)", posX, posY);
+  CGUIMessage message(GUI_MSG_GESTURE_NOTIFY, 0, 0, posX, posY);
+  if (g_windowManager.SendMessage(message))
+    return message.GetParam1();
+  
+  return EVENT_RESULT_UNHANDLED;
+}
+
+extern "C" bool XBMC_SetupDisplay()
+{
+  CLog::Log(LOGDEBUG, "XBMC_SetupDisplay()");
+  return g_application.getApplicationMessenger().SetupDisplay();
+}
+
+extern "C" bool XBMC_DestroyDisplay()
+{
+  CLog::Log(LOGDEBUG, "XBMC_DestroyDisplay()");
+  return g_application.getApplicationMessenger().DestroyDisplay();
 }
---
+++ xbmc/xbmc.h
@@ -0,0 +1,75 @@
+/*
+ *      Copyright (C) 2005-2009 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#pragma once
+
+// we need both here, one is for including into android, the other xbmc
+#include <stdint.h>
+#if defined(ANDROID) || defined(__ANDROID__)
+struct ANativeWindow;
+typedef struct ANativeWindow ANativeWindow;
+
+typedef int (*android_printf_t)(const char *format, ...);
+typedef int32_t (*android_setBuffersGeometry_t)(ANativeWindow* window_type, int32_t width, int32_t height, int32_t format);
+
+struct XBMC_PLATFORM {
+  const char    *log_name;
+  // android specific
+  ANativeWindow *window;
+  // callbacks from xbmc into android
+  // these are setup before call into XBMC_Initialize
+  android_printf_t android_printf;
+  android_setBuffersGeometry_t android_setBuffersGeometry;
+
+  // callbacks from android into xbmc
+  // these are valid after call into XBMC_Initialize
+
+};
+
+#else
+struct XBMC_PLATFORM {
+  const char    *log_name;
+};
+#endif
+typedef struct XBMC_PLATFORM XBMC_PLATFORM;
+
+
+typedef int (*XBMC_Run_t)(bool, XBMC_PLATFORM*);
+typedef void (*XBMC_Pause_t)(bool);
+typedef void (*XBMC_Stop_t)();
+typedef void (*XBMC_Key_t)(uint8_t, uint16_t, uint16_t, bool);
+typedef void (*XBMC_Touch_t)(uint8_t, uint8_t, uint16_t, uint16_t);
+typedef void (*XBMC_TouchGesture_t)(int32_t, float, float, float, float);
+typedef int (*XBMC_TouchGestureCheck_t)(float, float);
+
+typedef bool (*XBMC_SetupDisplay_t)();
+typedef bool (*XBMC_DestroyDisplay_t)();
+
+extern "C" int XBMC_Run(bool renderGUI = true, XBMC_PLATFORM* = NULL);
+extern "C" void XBMC_Pause(bool pause);
+extern "C" void XBMC_Stop();
+extern "C" void XBMC_Key(uint8_t code, uint16_t key, uint16_t modifiers, bool up);
+extern "C" void XBMC_Touch(uint8_t type, uint8_t button, uint16_t x, uint16_t y);
+extern "C" void XBMC_TouchGesture(int32_t action, float posX, float posY, float offsetX, float offsetY);
+extern "C" int XBMC_TouchGestureCheck(float posX, float posY);
+
+extern "C" bool XBMC_SetupDisplay();
+extern "C" bool XBMC_DestroyDisplay();
+
