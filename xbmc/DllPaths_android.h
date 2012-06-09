7b09cb3a582db53eaf7af8f818e5df89ced51f95
theuni <theuni-nospam-@xbmc.org>
2012-06-08 20:19:30 -0400

    [droid] fontconfig back to shared. breaks fonts when static
    This one makes no sense, we barely use fontconfig anywhere.

---
+++ xbmc/DllPaths_android.h
@@ -0,0 +1,88 @@
+
+/*
+ *      Copyright (C) 2005-2010 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+/* libraries */
+#pragma once
+#define DLL_PATH_CPLUFF        "special://libs/libcpluff.so"
+#define DLL_PATH_IMAGELIB      "special://libs/libImageLib.so"
+#define DLL_PATH_LIBEXIF       "special://libs/libexif.so"
+#define DLL_PATH_LIBID3TAG     "special://libs/libid3tag.so"
+#define DLL_PATH_LIBHDHOMERUN  "special://libs/libhdhomerun.so"
+#define DLL_PATH_MEDIAINFO     "special://libs/libmediainfo.so"
+#define DLL_PATH_LIBCMYTH      "special://libs/libcmyth.so"
+
+#define DLL_PATH_LIBRTMP       "librtmp.so"
+#define DLL_PATH_LIBNFS        "libnfs.so"
+#define DLL_PATH_LIBAFP        "libafpclient.so"
+#define DLL_PATH_LIBPLIST      "libplist.so"
+#define DLL_PATH_LIBSHAIRPORT  "libshairport.so"
+#define DLL_PATH_LIBCEC        ""
+
+#ifndef DLL_PATH_LIBCURL
+#define DLL_PATH_LIBCURL       "libcurl.so"
+#endif
+
+/* paplayer */
+#define DLL_PATH_AAC_CODEC     "special://libs/libAACCodec.so"
+#define DLL_PATH_ADPCM_CODEC   "special://libs/libadpcm.so"
+#define DLL_PATH_ADPLUG_CODEC  "special://libs/libadplug.so"
+#define DLL_PATH_APE_CODEC     "special://libs/libMACDll.so"
+#define DLL_PATH_ASAP_CODEC    "special://libs/libxbmc_asap.so"
+#define DLL_PATH_DCA_CODEC     "special://libs/libdcacodec.so"
+#define DLL_PATH_GYM_CODEC     "special://libs/libgensapu.so"
+#define DLL_PATH_MID_CODEC     "special://libs/libtimidity.so"
+#define DLL_PATH_MODULE_CODEC  "special://libs/libdumb.so"
+#define DLL_PATH_MPC_CODEC     "special://libs/libmpcdec.so"
+#define DLL_PATH_NSF_CODEC     "special://libs/libnosefart.so"
+#define DLL_PATH_SID_CODEC     "special://libs/libsidplay2.so"
+#define DLL_PATH_SPC_CODEC     "special://libs/libSNESAPU.so"
+#define DLL_PATH_VGM_CODEC     "special://libs/libvgmstream.so"
+#define DLL_PATH_WAVPACK_CODEC ""
+#define DLL_PATH_YM_CODEC      "special://libs/libstsoundlibrary.so"
+
+#define DLL_PATH_FLAC_CODEC    "libFLAC.so"
+#define DLL_PATH_MODPLUG_CODEC "libmodplug.so"
+#define DLL_PATH_OGG_CODEC     "libvorbisfile.so"
+
+/* dvdplayer */
+#define DLL_PATH_LIBASS        "libass.so"
+#define DLL_PATH_LIBDVDNAV     "special://libs/libdvdnav.so"
+#define DLL_PATH_LIBMPEG2      "libmpeg2.so"
+#define DLL_PATH_LIBMAD        "libmad.so"
+
+/* ffmpeg */
+#define DLL_PATH_LIBAVCODEC    "special://libs/libavcodec-53.so"
+#define DLL_PATH_LIBAVFORMAT   "special://libs/libavformat-53.so"
+#define DLL_PATH_LIBAVUTIL     "special://libs/libavutil-51.so"
+#define DLL_PATH_LIBPOSTPROC   "special://libs/libpostproc-52.so"
+#define DLL_PATH_LIBSWSCALE    "special://libs/libswscale-2.so"
+#define DLL_PATH_LIBAVFILTER   "special://libs/libavfilter-2.so"
+#define DLL_PATH_LIBSWRESAMPLE "special://libs/libswresample-0.so"
+
+/* cdrip */
+#define DLL_PATH_LAME_ENC      ""
+#define DLL_PATH_OGG           "libogg.so"
+#define DLL_PATH_VORBIS_ENC    "libvorbisenc.so"
+#define DLL_PATH_VORBIS        "libvorbis.so"
+
+/* libbluray */
+#define DLL_PATH_LIBBLURAY     "libbluray.so"
