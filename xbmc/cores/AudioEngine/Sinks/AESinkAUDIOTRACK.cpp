cb12e6fe7a980fee1f5e807ebc4038a75692269d
davilla <davilla@4pi.com>
2012-07-04 15:32:18 -0400

    [droid change AESinkAUDIOTRACK to use jni instead of direct accessing private api

--- xbmc/cores/AudioEngine/Sinks/AESinkAUDIOTRACK.cpp
+++ xbmc/cores/AudioEngine/Sinks/AESinkAUDIOTRACK.cpp
@@ -19,133 +19,82 @@
  *
  */
 
-#include "system.h"
-
 #include "AESinkAUDIOTRACK.h"
 #include "DynamicDll.h"
 #include "Utils/AEUtil.h"
 #include "Utils/AERingBuffer.h"
+#include "android/activity/XBMCApp.h"
 #include "utils/log.h"
 
-#include <stdint.h>
-#include <limits.h>
+#include <jni.h>
 
-#include <libaudiotrack_wrapper.h>
+static jint GetStaticIntField(JNIEnv *jenv, std::string class_name, std::string field_name)
+{
+  class_name.insert(0, "android/media/");
+  jclass cls = jenv->FindClass(class_name.c_str());
+  jfieldID field = jenv->GetStaticFieldID(cls, field_name.c_str(), "I");
+  jint int_field = jenv->GetStaticIntField(cls, field);
+  jenv->DeleteLocalRef(cls);
+  return int_field;
+}
 
-#define USE_BUFFER
+CAEDeviceInfo CAESinkAUDIOTRACK::m_info;
 ////////////////////////////////////////////////////////////////////////////////////////////
-class DllLibAudioTrackInterface
-{
-public:
-  virtual ~DllLibAudioTrackInterface() {}
-
-  virtual ATW_ctx ATW_create()=0;
-  virtual void    ATW_release(ATW_ctx ctx)=0;
-  virtual int     ATW_get_min_framecount(ATW_ctx ctx, int* frameCount, int sampleRate)=0;
-  virtual int     ATW_set(  ATW_ctx ctx, int channelCount, int sampleRate, int frameCount,
-                    ATWCallback cbf, void* cbf_ctx)=0;
-  virtual void    ATW_start(ATW_ctx ctx)=0;
-  virtual void    ATW_stop( ATW_ctx ctx)=0;
-  virtual int     ATW_write(ATW_ctx ctx, const void* buffer, size_t size)=0;
-  virtual void    ATW_flush(ATW_ctx ctx)=0;
-  virtual void    ATW_pause(ATW_ctx ctx)=0;
-  virtual void    ATW_mute( ATW_ctx ctx, int mute)=0;
-  virtual int     ATW_muted(ATW_ctx ctx)=0;
-  virtual void    ATW_set_volume(ATW_ctx ctx, float left, float right)=0;
-  virtual int     ATW_latency(ATW_ctx ctx)=0;
-};
-
-class DllLibAudioTrack : public DllDynamic, DllLibAudioTrackInterface
-{
-  DECLARE_DLL_WRAPPER(DllLibAudioTrack, "libaudiotrack_wrapper.so")
-
-  DEFINE_METHOD0(ATW_ctx, ATW_create)
-  DEFINE_METHOD1(void,    ATW_release,      (ATW_ctx p1))
-  DEFINE_METHOD3(int,     ATW_get_min_framecount,(ATW_ctx p1, int *p2, int p3))
-  DEFINE_METHOD6(int,     ATW_set,          (ATW_ctx p1, int p2, int p3, int p4,
-                                              ATWCallback p5, void *p6))
-  DEFINE_METHOD1(void,    ATW_start,        (ATW_ctx p1))
-  DEFINE_METHOD1(void,    ATW_stop,         (ATW_ctx p1))
-  DEFINE_METHOD3(int,     ATW_write,        (ATW_ctx p1, const void* p2, size_t p3))
-  DEFINE_METHOD1(void,    ATW_flush,        (ATW_ctx p1))
-  DEFINE_METHOD1(void,    ATW_pause,        (ATW_ctx p1))
-  DEFINE_METHOD2(void,    ATW_mute,         (ATW_ctx p1, int p2))
-  DEFINE_METHOD1(int,     ATW_muted,        (ATW_ctx p1))
-  DEFINE_METHOD3(void,    ATW_set_volume,   (ATW_ctx p1, float p2, float p3))
-  DEFINE_METHOD1(int,     ATW_latency,      (ATW_ctx p1))
-
-  BEGIN_METHOD_RESOLVE()
-    RESOLVE_METHOD(ATW_create)
-    RESOLVE_METHOD(ATW_release)
-    RESOLVE_METHOD(ATW_get_min_framecount)
-    RESOLVE_METHOD(ATW_set)
-    RESOLVE_METHOD(ATW_start)
-    RESOLVE_METHOD(ATW_stop)
-    RESOLVE_METHOD(ATW_write)
-    RESOLVE_METHOD(ATW_flush)
-    RESOLVE_METHOD(ATW_pause)
-    RESOLVE_METHOD(ATW_mute)
-    RESOLVE_METHOD(ATW_muted)
-    RESOLVE_METHOD(ATW_set_volume)
-    RESOLVE_METHOD(ATW_latency)
-  END_METHOD_RESOLVE()
-};
-
-DllLibAudioTrack *CAESinkAUDIOTRACK::m_dll = NULL;
-
 CAESinkAUDIOTRACK::CAESinkAUDIOTRACK()
+  : CThread("audiotrack")
 {
-  CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::CAESinkAUDIOTRACK");
-  if (!m_dll)
-  {
-    m_dll = new DllLibAudioTrack;
-    m_dll->Load();
-  }
   m_buffer = NULL;
-  m_ATWrapper = m_dll->ATW_create();
+
+  CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::CAESinkAUDIOTRACK");
 }
 
 CAESinkAUDIOTRACK::~CAESinkAUDIOTRACK()
 {
   CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::~CAESinkAUDIOTRACK");
-  //delete m_dll, m_dll = NULL;
 }
 
 bool CAESinkAUDIOTRACK::Initialize(AEAudioFormat &format, std::string &device)
 {
   CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::Initialize");
-  if (!m_ATWrapper)
-    return false;
-
-  //format.m_sampleRate = (format.m_sampleRate = 44100) ? 44100 : 48000;
-  format.m_sampleRate = 48000;
-  format.m_dataFormat = AE_FMT_S16LE;
-
-  CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::Initialize2");
-  int min_frames;
-  m_dll->ATW_get_min_framecount(m_ATWrapper, &min_frames, format.m_sampleRate);
-
-  CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::Initialize3, min_frames(%d)", min_frames);
-  format.m_frames         = min_frames;
-  format.m_frameSamples   = format.m_channelLayout.Count();
-  format.m_frameSize      = format.m_frameSamples * (CAEUtil::DataFormatToBits(format.m_dataFormat) >> 3);
-#ifdef USE_BUFFER
-  m_dll->ATW_set(m_ATWrapper, format.m_frameSamples, format.m_sampleRate, format.m_frames,
-    AudioTrackCallback, this);
-#else
-  m_dll->ATW_set(m_ATWrapper, format.m_frameSamples, format.m_sampleRate, format.m_frames,
-    NULL, NULL);
-#endif
 
   m_format = format;
-  m_started = false;
-  m_draining = false;
-  m_SecondsPerByte = 1.0 / (double)(m_format.m_sampleRate * m_format.m_frameSize);
-#ifdef USE_BUFFER
-  m_buffer = new AERingBuffer(m_format.m_frameSize * m_format.m_sampleRate);
-#endif
+  // default to 44100, all android devices support it.
+  // then check if we can support the requested rate.
+  unsigned int sampleRate = 44100;
+  for (size_t i = 0; i < m_info.m_sampleRates.size(); i++)
+  {
+    if (m_format.m_sampleRate == m_info.m_sampleRates[i])
+    {
+      sampleRate = format.m_sampleRate;
+      break;
+    }
+  }
+  m_format.m_sampleRate   = sampleRate;
+  m_format.m_dataFormat   = AE_FMT_S16LE;
+  m_format.m_frameSamples = m_format.m_channelLayout.Count();
+  m_format.m_frameSize    = m_format.m_frameSamples * (CAEUtil::DataFormatToBits(m_format.m_dataFormat) >> 3);
 
-  m_dll->ATW_start(m_ATWrapper), m_started = true;
+  // launch the process thread and wait for the
+  // AutoTrack jni object to get created and setup.
+  m_inited.Reset();
+  Create();
+  if(!m_inited.WaitMSec(100))
+  {
+    while(!m_inited.WaitMSec(1))
+      sleep(10);
+  }
+
+  // m_init_frames is volatile and has been setup by Process()
+  m_format.m_frames = m_init_frames;
+
+  m_SecondsPerByte = 1.0 / (double)(m_format.m_frameSize * m_format.m_sampleRate);
+  m_buffer = new AERingBuffer(m_format.m_frameSize * m_format.m_sampleRate);
+
+  CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::Initialize, "
+    "m_SecondsPerByte(%f), m_buffer->GetMaxSize(%d), m_buffer_sec(%f), m_latency_sec(%f)",
+    m_SecondsPerByte, m_buffer->GetMaxSize(), m_buffer_sec, m_latency_sec);
+
+  format = m_format;
 
   return true;
 }
@@ -153,8 +102,7 @@ bool CAESinkAUDIOTRACK::Initialize(AEAudioFormat &format, std::string &device)
 void CAESinkAUDIOTRACK::Deinitialize()
 {
   CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::Deinitialize");
-  m_dll->ATW_stop(m_ATWrapper), m_started = false;
-  m_dll->ATW_release(m_ATWrapper), m_ATWrapper = NULL;
+  StopThread();
   delete m_buffer, m_buffer = NULL;
 }
 
@@ -167,73 +115,50 @@ bool CAESinkAUDIOTRACK::IsCompatible(const AEAudioFormat format, const std::stri
   CLog::Log(LOGDEBUG, "  is Sample Format : %s", CAEUtil::DataFormatToStr(m_format.m_dataFormat));
   CLog::Log(LOGDEBUG, "  is Channel Count : %d", m_format.m_channelLayout.Count());
   CLog::Log(LOGDEBUG, "  is Channel Layout: %s", ((std::string)m_format.m_channelLayout).c_str());
-  CLog::Log(LOGDEBUG, "  is Frames        : %d", m_format.m_frames);
-  CLog::Log(LOGDEBUG, "  is Frame Samples : %d", m_format.m_frameSamples);
-  CLog::Log(LOGDEBUG, "  is Frame Size    : %d", m_format.m_frameSize);
 
   CLog::Log(LOGDEBUG, "  in Output Device : %s", device.c_str());
   CLog::Log(LOGDEBUG, "  in Sample Rate   : %d", in_format.m_sampleRate);
   CLog::Log(LOGDEBUG, "  in Sample Format : %s", CAEUtil::DataFormatToStr(in_format.m_dataFormat));
   CLog::Log(LOGDEBUG, "  in Channel Count : %d", in_format.m_channelLayout.Count());
   CLog::Log(LOGDEBUG, "  is Channel Layout: %s", ((std::string)in_format.m_channelLayout).c_str());
-  CLog::Log(LOGDEBUG, "  in Frames        : %d", in_format.m_frames);
-  CLog::Log(LOGDEBUG, "  in Frame Samples : %d", in_format.m_frameSamples);
-  CLog::Log(LOGDEBUG, "  in Frame Size    : %d", in_format.m_frameSize);
 
   return ((m_format.m_sampleRate    == format.m_sampleRate) &&
           (m_format.m_dataFormat    == format.m_dataFormat) &&
           (m_format.m_channelLayout == format.m_channelLayout));
 }
 
-void CAESinkAUDIOTRACK::Stop()
-{
-  m_dll->ATW_stop(m_ATWrapper), m_started = false;
-}
-
 double CAESinkAUDIOTRACK::GetDelay()
 {
-#ifdef USE_BUFFER
-  double latency_sec = (double)m_dll->ATW_latency(m_ATWrapper) / 1000.0;
-  // returns the time it takes to play a packet if we add one at this time
-  //double seconds_to_empty = m_SecondsPerByte * (double)m_buffer->GetReadSize();
-  //CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::GetDelay, latency_sec(%f), buffer_sec(%f)",
-  //  latency_sec, buffer_sec);
-  //return seconds_to_empty + latency_sec;
-  return latency_sec;
-#else
-  double latency_sec = (double)m_dll->ATW_latency(m_ATWrapper) / 1000.0;
-  return latency_sec;0
-#endif
+  // this includes any latency due to AudioTrack buffer size,
+  // AudioMixer (if any) and audio hardware driver.
+  double seconds_to_empty = m_SecondsPerByte * (double)m_buffer->GetReadSize();
+  //CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::GetDelay, seconds_to_empty(%f), m_latency_sec(%f)",
+  //  seconds_to_empty, m_latency_sec);
+  return seconds_to_empty + m_latency_sec;
 }
 
 double CAESinkAUDIOTRACK::GetCacheTime()
 {
-  // returns total amount of data cached in audio output at this time
-#ifdef USE_BUFFER
+  // returns the time in seconds that it will take
+  // to underrun the buffer if no sample is added.
+
   double seconds_to_empty = m_SecondsPerByte * (double)m_buffer->GetReadSize();
-  //CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::GetCacheTime, seconds_to_empty(%f)", seconds_to_empty);
+  //CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::GetCacheTime, seconds_to_empty(%f)",
+  //  seconds_to_empty);
   return seconds_to_empty;
-#else
-  return 0;
-#endif
 }
 
 double CAESinkAUDIOTRACK::GetCacheTotal()
 {
-  // total amount the audio sink can buffer
-#ifdef USE_BUFFER
-  float buffer_totalsec = (double)m_buffer->GetMaxSize() / (double)m_format.m_sampleRate;
-  //CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::GetCacheTotal, buffer_sec(%f)", buffer_totalsec);
+  // total amount that the audio sink can buffer in units of seconds
+  double buffer_totalsec = (double)m_buffer->GetMaxSize() / (double)m_format.m_sampleRate;
+  //CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::GetCacheTotal, buffer_totalsec(%f)",
+  //  buffer_totalsec);
   return buffer_totalsec;
-#else
-  return 0;
-#endif
 }
 
 unsigned int CAESinkAUDIOTRACK::AddPackets(uint8_t *data, unsigned int frames)
 {
-#ifdef USE_BUFFER
-  //CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::AddPackets, frames(%d)", frames);
   unsigned int room = m_buffer->GetWriteSize();
   unsigned int addbytes = frames * m_format.m_frameSize;
   unsigned int bytes_per_second = m_format.m_frameSize * m_format.m_sampleRate;
@@ -241,9 +166,6 @@ unsigned int CAESinkAUDIOTRACK::AddPackets(uint8_t *data, unsigned int frames)
   if (m_draining)
     return frames;
 
-  if (!m_started)
-    m_dll->ATW_start(m_ATWrapper), m_started = true;
- 
   unsigned int total_ms_sleep = 0;
   while (!m_draining && addbytes > room && total_ms_sleep < 200)
   {
@@ -267,49 +189,161 @@ unsigned int CAESinkAUDIOTRACK::AddPackets(uint8_t *data, unsigned int frames)
   else
     m_buffer->Write(data, addbytes);
 
-  // ms sleep block
-  Sleep(m_dll->ATW_latency(m_ATWrapper)/2);
-
   return frames;
-#else
-  m_dll->ATW_write(m_ATWrapper, data, frames * m_format.m_frameSize);
-  return frames;
-#endif
 }
 
 void CAESinkAUDIOTRACK::Drain()
 {
   CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::Drain");
   m_draining = true;
-  m_dll->ATW_stop( m_ATWrapper), m_started = false;
-  m_dll->ATW_flush(m_ATWrapper);
 }
 
 void CAESinkAUDIOTRACK::EnumerateDevicesEx(AEDeviceInfoList &list)
 {
-  CAEDeviceInfo info;
+  m_info.m_channels.Reset();
+  m_info.m_dataFormats.clear();
+  m_info.m_sampleRates.clear();
 
-  info.m_channels.Reset();
-  info.m_dataFormats.clear();
-  info.m_sampleRates.clear();
+  m_info.m_deviceType = AE_DEVTYPE_PCM;
+  m_info.m_deviceName = "AudioTrack";
+  m_info.m_displayName = "android";
+  m_info.m_displayNameExtra = "audiotrack";
+  m_info.m_channels += AE_CH_FL;
+  m_info.m_channels += AE_CH_FR;
+  m_info.m_sampleRates.push_back(44100);
+  m_info.m_sampleRates.push_back(48000);
+  m_info.m_dataFormats.push_back(AE_FMT_S16LE);
 
-  info.m_deviceType = AE_DEVTYPE_PCM;
-  info.m_deviceName = "AudioTrack";
-  info.m_displayName = "android";
-  info.m_displayNameExtra = "default display";
-  info.m_channels += AE_CH_FL;
-  info.m_channels += AE_CH_FR;
-  //info.m_sampleRates.push_back(44100);
-  info.m_sampleRates.push_back(48000);
-  info.m_dataFormats.push_back(AE_FMT_S16LE);
-
-  list.push_back(info);
+  list.push_back(m_info);
 }
 
-int CAESinkAUDIOTRACK::AudioTrackCallback(void *ctx, void *buffer, size_t size)
+void CAESinkAUDIOTRACK::ProbeSupportedSampleRates()
 {
-  CAESinkAUDIOTRACK *at_ctx = (CAESinkAUDIOTRACK*)ctx;
-  unsigned int readsize = std::min(at_ctx->m_buffer->GetReadSize(), size);
-  at_ctx->m_buffer->Read((unsigned char*)buffer, readsize);
-  return readsize;
+  m_info.m_sampleRates.push_back(44100);
+  m_info.m_sampleRates.push_back(48000);
+}
+
+void CAESinkAUDIOTRACK::OnStartup()
+{
+}
+
+void CAESinkAUDIOTRACK::OnExit()
+{
+}
+
+void CAESinkAUDIOTRACK::Process()
+{
+  CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::Process");
+  JNIEnv *jenv = NULL;
+
+  CXBMCApp::AttachCurrentThread(&jenv, NULL);
+  jenv->PushLocalFrame(4);
+
+  jclass cls = jenv->FindClass("android/media/AudioTrack");
+  jclass jcAudioTrack = static_cast<jclass>(jenv->NewGlobalRef(cls));
+  jenv->DeleteLocalRef(cls);
+
+  jmethodID jmInit              = jenv->GetMethodID(jcAudioTrack, "<init>", "(IIIIII)V");
+  jmethodID jmPlay              = jenv->GetMethodID(jcAudioTrack, "play", "()V");
+  //jmethodID jmPause           = jenv->GetMethodID(jcAudioTrack, "pause", "()V");
+  jmethodID jmStop              = jenv->GetMethodID(jcAudioTrack, "stop", "()V");
+  jmethodID jmRelease           = jenv->GetMethodID(jcAudioTrack, "release", "()V");
+  jmethodID jmWrite             = jenv->GetMethodID(jcAudioTrack, "write", "([BII)I");
+  //jmethodID jmGetPos          = jenv->GetMethodID(jcAudioTrack, "getPlaybackHeadPosition", "()I");
+  jmethodID jmGetMinBufferSize  = jenv->GetStaticMethodID(jcAudioTrack, "getMinBufferSize", "(III)I");
+
+  jint channelConfig;
+  if (m_format.m_frameSamples == 1)
+    channelConfig = GetStaticIntField(jenv, "AudioFormat", "CHANNEL_OUT_MONO");
+  else
+    channelConfig = GetStaticIntField(jenv, "AudioFormat", "CHANNEL_OUT_STEREO");
+
+  jint audioFormat;
+  if (CAEUtil::DataFormatToBits(m_format.m_dataFormat) == 8)
+    audioFormat = GetStaticIntField(jenv, "AudioFormat", "ENCODING_PCM_8BIT");
+  else
+    audioFormat = GetStaticIntField(jenv, "AudioFormat", "ENCODING_PCM_16BIT");
+
+  jint sampleRate = m_format.m_sampleRate;
+
+  jint min_buffer_size = jenv->CallStaticIntMethod(jcAudioTrack, jmGetMinBufferSize,
+    sampleRate, channelConfig, audioFormat);
+
+  CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::Process:jmGetMinBufferSize, "
+    "min_buffer_size(%d), sampleRate(%d), channelConfig(%d), audioFormat(%d)",
+    min_buffer_size, sampleRate, channelConfig, audioFormat);
+
+  // double it get better performance
+  min_buffer_size *= 2;
+  m_init_frames = min_buffer_size / m_format.m_frameSize;
+  m_format.m_frames = m_init_frames;
+
+  m_buffer_sec  = (double)m_init_frames / m_format.m_sampleRate;
+  // we cannot get actual latency using jni so guess
+  m_latency_sec = 0.100;
+  CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::Process, m_init_frames(%d), m_buffer_sec(%f), m_latency_sec(%f)",
+    m_init_frames, m_buffer_sec, m_latency_sec);
+
+  CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::Process3");
+  jobject joAudioTrack = jenv->NewObject(jcAudioTrack, jmInit,
+    GetStaticIntField(jenv, "AudioManager", "STREAM_MUSIC"),
+    sampleRate,
+    channelConfig,
+    audioFormat,
+    min_buffer_size,
+    GetStaticIntField(jenv, "AudioTrack", "MODE_STREAM"));
+
+  CLog::Log(LOGDEBUG, "CAESinkAUDIOTRACK::Process4, joAudioTrack(%p)", joAudioTrack);
+
+  // The AudioTrack object has been created and waiting to play,
+  m_inited.Set();
+  // yield to give other threads a chance to do some work.
+  sched_yield();
+
+  jarray jbuffer = jenv->NewByteArray(min_buffer_size);
+
+  m_draining = false;
+  jenv->CallVoidMethod(joAudioTrack, jmPlay, m_started = true);
+
+  while (!m_bStop)
+  {
+    //int bytes_to_empty = jenv->CallIntMethod(joAudioTrack, jmGetPos);
+    //bytes_to_empty *= m_format.m_frameSize;
+
+    unsigned int read_frames = m_buffer->GetReadSize() / m_format.m_frameSize;
+    if (read_frames >= m_format.m_frames)
+    {
+      // Stream the next batch of audio data to the Java AudioTrack.
+      void *pBuffer = jenv->GetPrimitiveArrayCritical(jbuffer, NULL);
+      if (pBuffer)
+      {
+        unsigned int buffer_size = read_frames * m_format.m_frameSize;
+        m_buffer->Read((unsigned char*)pBuffer, buffer_size);
+        jenv->ReleasePrimitiveArrayCritical(jbuffer, pBuffer, 0);
+        jenv->CallIntMethod(joAudioTrack, jmWrite, jbuffer, 0, buffer_size);
+      }
+      else
+      {
+        CLog::Log(LOGDEBUG, "Failed to get pointer to array bytes");
+      }
+      // yield this audio thread to give other threads a chance to do some work.
+      sched_yield();
+    }
+    else
+      Sleep(10);
+  }
+
+  jenv->CallVoidMethod(joAudioTrack, jmStop), m_started = false;
+  jenv->CallVoidMethod(joAudioTrack, jmRelease);
+
+  // might toss an exception on jmRelease so catch it.
+  jthrowable exception = jenv->ExceptionOccurred();
+  if (exception)
+  {
+    jenv->ExceptionDescribe();
+    jenv->ExceptionClear();
+  }
+
+  jenv->PopLocalFrame(NULL);
+  CXBMCApp::DetachCurrentThread();
 }
