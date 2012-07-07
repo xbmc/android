051fbdc149ba58689fdce82e463262bfa4ae3bfa
montellese <montellese@xbmc.org>
2012-07-07 23:56:21 +0200

    [droid]: android_main: cleanup JNI-related variable names

--- xbmc/android/activity/android_main.cpp
+++ xbmc/android/activity/android_main.cpp
@@ -36,69 +36,69 @@ void setup_env(struct android_app* state)
   const char* temp;
 
   vm->AttachCurrentThread(&env, NULL);
-  jobject activity = state->activity->clazz;
-  jclass activityClass = env->GetObjectClass(activity);
+  jobject oActivity = state->activity->clazz;
+  jclass cActivity = env->GetObjectClass(oActivity);
 
   // get the path to the android system libraries
-  jclass jSystem = env->FindClass("java/lang/System");
-  jmethodID systemLib = env->GetStaticMethodID(jSystem, "getProperty", "(Ljava/lang/String;)Ljava/lang/String;");
-  jstring jSystemProperty = env->NewStringUTF("java.library.path");
-  jstring jSystemLib = (jstring)env->CallStaticObjectMethod(jSystem, systemLib, jSystemProperty);
-  temp = env->GetStringUTFChars(jSystemLib, NULL);
+  jclass cSystem = env->FindClass("java/lang/System");
+  jmethodID midSystemGetProperty = env->GetStaticMethodID(cSystem, "getProperty", "(Ljava/lang/String;)Ljava/lang/String;");
+  jstring sJavaLibraryPath = env->NewStringUTF("java.library.path");
+  jstring sSystemLibraryPath = (jstring)env->CallStaticObjectMethod(cSystem, midSystemGetProperty, sJavaLibraryPath);
+  temp = env->GetStringUTFChars(sSystemLibraryPath, NULL);
   setenv("XBMC_ANDROID_SYSTEM_LIBS", temp, 0);
-  env->ReleaseStringUTFChars(jSystemLib, temp);
-  env->DeleteLocalRef(jSystemLib);
-  env->DeleteLocalRef(jSystem);
-  env->DeleteLocalRef(jSystemProperty);
+  env->ReleaseStringUTFChars(sSystemLibraryPath, temp);
+  env->DeleteLocalRef(sJavaLibraryPath);
+  env->DeleteLocalRef(sSystemLibraryPath);
+  env->DeleteLocalRef(cSystem);
 
   // get the path to XBMC's data directory (usually /data/data/<app-name>)
-  jmethodID getApplicationInfo = env->GetMethodID(activityClass, "getApplicationInfo", "()Landroid/content/pm/ApplicationInfo;");
-  jobject jApplicationInfo = env->CallObjectMethod(activity, getApplicationInfo);
-  jclass applicationInfoClass = env->GetObjectClass(jApplicationInfo);
-  jfieldID dataDir = env->GetFieldID(applicationInfoClass, "dataDir", "Ljava/lang/String;");
-  jstring jDataDir = (jstring)env->GetObjectField(jApplicationInfo, dataDir);
-  temp = env->GetStringUTFChars(jDataDir, NULL);
+  jmethodID midActivityGetApplicationInfo = env->GetMethodID(cActivity, "getApplicationInfo", "()Landroid/content/pm/ApplicationInfo;");
+  jobject oApplicationInfo = env->CallObjectMethod(oActivity, midActivityGetApplicationInfo);
+  jclass cApplicationInfo = env->GetObjectClass(oApplicationInfo);
+  jfieldID fidApplicationInfoDataDir = env->GetFieldID(cApplicationInfo, "dataDir", "Ljava/lang/String;");
+  jstring sDataDir = (jstring)env->GetObjectField(oApplicationInfo, fidApplicationInfoDataDir);
+  temp = env->GetStringUTFChars(sDataDir, NULL);
   setenv("XBMC_ANDROID_DATA", temp, 0);
-  env->ReleaseStringUTFChars(jDataDir, temp);
-  env->DeleteLocalRef(jDataDir);
+  env->ReleaseStringUTFChars(sDataDir, temp);
+  env->DeleteLocalRef(sDataDir);
   
   // get the path to where android extracts native libraries to
-  jfieldID nativeLibraryDir = env->GetFieldID(applicationInfoClass, "nativeLibraryDir", "Ljava/lang/String;");
-  jstring jNativeLibraryDir = (jstring)env->GetObjectField(jApplicationInfo, nativeLibraryDir);
-  temp = env->GetStringUTFChars(jNativeLibraryDir, NULL);
+  jfieldID fidApplicationInfoNativeLibraryDir = env->GetFieldID(cApplicationInfo, "nativeLibraryDir", "Ljava/lang/String;");
+  jstring sNativeLibraryDir = (jstring)env->GetObjectField(oApplicationInfo, fidApplicationInfoNativeLibraryDir);
+  temp = env->GetStringUTFChars(sNativeLibraryDir, NULL);
   setenv("XBMC_ANDROID_LIBS", temp, 0);
-  env->ReleaseStringUTFChars(jNativeLibraryDir, temp);
-  env->DeleteLocalRef(jNativeLibraryDir);
-  env->DeleteLocalRef(applicationInfoClass);
-  env->DeleteLocalRef(jApplicationInfo);
+  env->ReleaseStringUTFChars(sNativeLibraryDir, temp);
+  env->DeleteLocalRef(sNativeLibraryDir);
+  env->DeleteLocalRef(oApplicationInfo);
+  env->DeleteLocalRef(cApplicationInfo);
 
   // get the path to the APK
   char apkPath[PATH_MAX];
-  jmethodID getPackageResourcePath = env->GetMethodID(activityClass, "getPackageResourcePath", "()Ljava/lang/String;");
-  jstring jApkPath = (jstring)env->CallObjectMethod(activity, getPackageResourcePath);
-  temp = env->GetStringUTFChars(jApkPath, NULL);
+  jmethodID midActivityGetPackageResourcePath = env->GetMethodID(cActivity, "getPackageResourcePath", "()Ljava/lang/String;");
+  jstring sApkPath = (jstring)env->CallObjectMethod(oActivity, midActivityGetPackageResourcePath);
+  temp = env->GetStringUTFChars(sApkPath, NULL);
   strcpy(apkPath, temp);
   setenv("XBMC_ANDROID_APK", apkPath, 0);
-  env->ReleaseStringUTFChars(jApkPath, temp);
-  env->DeleteLocalRef(jApkPath);
+  env->ReleaseStringUTFChars(sApkPath, temp);
+  env->DeleteLocalRef(sApkPath);
   
   // Get the path to the temp/cache directory
   char cacheDir[PATH_MAX];
   char tempDir[PATH_MAX];
-  jmethodID getCacheDir = env->GetMethodID(activityClass, "getCacheDir", "()Ljava/io/File;");
-  jobject jCacheDir = env->CallObjectMethod(activity, getCacheDir);
+  jmethodID midActivityGetCacheDir = env->GetMethodID(cActivity, "getCacheDir", "()Ljava/io/File;");
+  jobject oCacheDir = env->CallObjectMethod(oActivity, midActivityGetCacheDir);
 
-  jclass fileClass = env->GetObjectClass(jCacheDir);
-  jmethodID getAbsolutePath = env->GetMethodID(fileClass, "getAbsolutePath", "()Ljava/lang/String;");
-  env->DeleteLocalRef(fileClass);
+  jclass cFile = env->GetObjectClass(oCacheDir);
+  jmethodID midFileGetAbsolutePath = env->GetMethodID(cFile, "getAbsolutePath", "()Ljava/lang/String;");
+  env->DeleteLocalRef(cFile);
 
-  jstring jCachePath = (jstring)env->CallObjectMethod(jCacheDir, getAbsolutePath);
-  temp = env->GetStringUTFChars(jCachePath, NULL);
+  jstring sCachePath = (jstring)env->CallObjectMethod(oCacheDir, midFileGetAbsolutePath);
+  temp = env->GetStringUTFChars(sCachePath, NULL);
   strcpy(cacheDir, temp);
   strcpy(tempDir, temp);
-  env->ReleaseStringUTFChars(jCachePath, temp);
-  env->DeleteLocalRef(jCachePath);
-  env->DeleteLocalRef(jCacheDir);
+  env->ReleaseStringUTFChars(sCachePath, temp);
+  env->DeleteLocalRef(sCachePath);
+  env->DeleteLocalRef(oCacheDir);
 
   strcat(tempDir, "/temp");
   setenv("XBMC_TEMP", tempDir, 0);
@@ -112,46 +112,47 @@ void setup_env(struct android_app* state)
   setenv("XBMC_BIN_HOME", cacheDir, 0);
   setenv("XBMC_HOME"    , cacheDir, 0);
 
-  // Get the path to the external storage by employing JNI
+  // Get the path to the external storage
   // The path would actually be available from state->activity->externalDataPath (apart from a (known) bug in Android 2.3.x)
-  // but calling getExternalFilesDir() will automatically create the necessary directories for us. We don't seem to have the
-  // rights to create a directory in /mnt/sdcard/Android/data/ ourselfs.
+  // but calling getExternalFilesDir() will automatically create the necessary directories for us.
   char storagePath[PATH_MAX];
-  jmethodID getExternalFilesDir = env->GetMethodID(activityClass, "getExternalFilesDir", "(Ljava/lang/String;)Ljava/io/File;");
-  jobject jExternalDir = env->CallObjectMethod(activity, getExternalFilesDir, (jstring)NULL);
+  jmethodID midActivityGetExternalFilesDir = env->GetMethodID(cActivity, "getExternalFilesDir", "(Ljava/lang/String;)Ljava/io/File;");
+  jobject oExternalDir = env->CallObjectMethod(oActivity, midActivityGetExternalFilesDir, (jstring)NULL);
 
-  jstring jExternalPath = (jstring)env->CallObjectMethod(jExternalDir, getAbsolutePath);
-  temp = env->GetStringUTFChars(jExternalPath, NULL);
+  jstring sExternalPath = (jstring)env->CallObjectMethod(oExternalDir, midFileGetAbsolutePath);
+  temp = env->GetStringUTFChars(sExternalPath, NULL);
   strcpy(storagePath, temp);
-  env->ReleaseStringUTFChars(jExternalPath, temp);
-  env->DeleteLocalRef(jExternalPath);
-  env->DeleteLocalRef(jExternalDir);
+  env->ReleaseStringUTFChars(sExternalPath, temp);
+  env->DeleteLocalRef(sExternalPath);
+  env->DeleteLocalRef(oExternalDir);
 
   // Check if we don't have a valid path yet
   if (strlen(storagePath) <= 0)
   {
-    // Get the path to the internal storage by employing JNI
+    // Get the path to the internal storage
     // For more details see the comment on getting the path to the external storage
-    jstring jstrName = env->NewStringUTF("org.xbmc.xbmc");
-    jmethodID getDir = env->GetMethodID(activityClass, "getDir", "(Ljava/lang/String;I)Ljava/io/File;");
-    jobject jInternalDir = env->CallObjectMethod(activity, getDir, jstrName, 1 /* MODE_WORLD_READABLE */);
-    env->DeleteLocalRef(jstrName);
+    jstring sXbmcName = env->NewStringUTF("org.xbmc.xbmc");
+    jmethodID midActivityGetDir = env->GetMethodID(cActivity, "getDir", "(Ljava/lang/String;I)Ljava/io/File;");
+    jobject oInternalDir = env->CallObjectMethod(oActivity, midActivityGetDir, sXbmcName, 1 /* MODE_WORLD_READABLE */);
+    env->DeleteLocalRef(sXbmcName);
 
-    jstring jInternalPath = (jstring)env->CallObjectMethod(jInternalDir, getAbsolutePath);
-    temp = env->GetStringUTFChars(jInternalPath, NULL);
+    jstring sInternalPath = (jstring)env->CallObjectMethod(oInternalDir, midFileGetAbsolutePath);
+    temp = env->GetStringUTFChars(sInternalPath, NULL);
     strcpy(storagePath, temp);
-    env->ReleaseStringUTFChars(jInternalPath, temp);
-    env->DeleteLocalRef(jInternalPath);
-    env->DeleteLocalRef(jInternalDir);
+    env->ReleaseStringUTFChars(sInternalPath, temp);
+    env->DeleteLocalRef(sInternalPath);
+    env->DeleteLocalRef(oInternalDir);
   }
-  
-  env->DeleteLocalRef(activityClass);
+
+  env->DeleteLocalRef(cActivity);
 
   // Check if we have a valid home path
   if (strlen(storagePath) > 0)
     setenv("HOME", storagePath, 0);
   else
     setenv("HOME", getenv("XBMC_TEMP"), 0);
+
+  state->activity->vm->DetachCurrentThread();
 }
 
 extern void android_main(struct android_app* state)
@@ -168,7 +169,6 @@ extern void android_main(struct android_app* state)
     CXBMCApp::android_printf("android_main: setup failed");
 
   CXBMCApp::android_printf("android_main: Exiting");
-  state->activity->vm->DetachCurrentThread();
   // We need to call exit() so that all loaded libraries are properly unloaded
   // otherwise on the next start of the Activity android will simple re-use
   // those loaded libs in the state they were in when we quit XBMC last time
