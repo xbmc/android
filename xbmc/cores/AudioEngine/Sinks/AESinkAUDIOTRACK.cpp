da171272d940036c85ddd77e3356bd3033da9d29
davilla <davilla@4pi.com>
2012-07-08 12:19:37 -0400

    [droid] remove redundent check for neon, AE_FMT_FLOAT is only enabled  if you have neon so you do not need to check each time you get an  audio packet, it is not going to be changing on the fly

--- xbmc/cores/AudioEngine/Sinks/AESinkAUDIOTRACK.cpp
+++ xbmc/cores/AudioEngine/Sinks/AESinkAUDIOTRACK.cpp
@@ -197,15 +197,12 @@ unsigned int CAESinkAUDIOTRACK::AddPackets(uint8_t *data, unsigned int frames)
         break;
 #if defined(__ARM_NEON__)
       case AE_FMT_FLOAT:
-        if (g_cpuInfo.GetCPUFeatures() & CPU_FEATURE_NEON)
-        {
-          if (!m_alignedS16LE)
-            m_alignedS16LE = (int16_t*)_aligned_malloc(m_format.m_frames * m_sink_frameSize, 16);
-          // neon convert AE_FMT_S16LE to AE_FMT_FLOAT
-          pa_sconv_s16le_from_f32ne_neon(write_frames * m_format.m_frameSamples, (const float*)data, m_alignedS16LE);
-          m_sinkbuffer->Write((unsigned char*)m_alignedS16LE, write_frames * m_sink_frameSize);
-          m_wake.Set();
-        }
+        if (!m_alignedS16LE)
+          m_alignedS16LE = (int16_t*)_aligned_malloc(m_format.m_frames * m_sink_frameSize, 16);
+        // neon convert AE_FMT_S16LE to AE_FMT_FLOAT
+        pa_sconv_s16le_from_f32ne_neon(write_frames * m_format.m_frameSamples, (const float*)data, m_alignedS16LE);
+        m_sinkbuffer->Write((unsigned char*)m_alignedS16LE, write_frames * m_sink_frameSize);
+        m_wake.Set();
         break;
 #endif
       default:
