bfbe9fded397d1cf84861824333cd5f7163b1da9
Cory Fields <theuni-nospam-@xbmc.org>
2012-07-12 01:27:22 -0400

    [droid] add our big mess of android activity files

---
+++ xbmc/android/activity/CPUFeatures.cpp
@@ -0,0 +1,29 @@
+/*
+ *      Copyright (C) 2012 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "CPUFeatures.h"
+#include <cpu-features.h>
+
+bool CAndroidCPU::HasNeon()
+{
+  if (android_getCpuFamily() == ANDROID_CPU_FAMILY_ARM) 
+    return ((android_getCpuFeatures() & ANDROID_CPU_ARM_FEATURE_NEON) != 0);
+  return false;
+}
