xbmc/android

commit e9ea5f7178e38b686c9d508c6fddccfadead151f

[droid] Make (some of) our neon optims detectable at runtime

xbmc/cores/VideoRenderers/LinuxRendererGLES.cpp
@@ -50,6 +50,7 @@
 
 #if defined(__ARM_NEON__)
 #include "yuv2rgb.neon.h"
+#include "utils/CPUInfo.h"
 #endif
 #ifdef HAVE_VIDEOTOOLBOXDECODER
 #include "DVDCodecs/Video/DVDVideoCodecVideoToolBox.h"
@@ -1339,9 +1340,13 @@ void CLinuxRendererGLES::UploadYV12Texture(int source)
     }
 
 #if defined(__ARM_NEON__)
-    yuv420_2_rgb8888_neon(m_rgbBuffer, im->plane[0], im->plane[2], im->plane[1],
-      m_sourceWidth, m_sourceHeight, im->stride[0], im->stride[1], m_sourceWidth * 4);
-#else
+    if (g_cpuInfo.GetCPUFeatures() & CPU_FEATURE_NEON)
+    {
+      yuv420_2_rgb8888_neon(m_rgbBuffer, im->plane[0], im->plane[2], im->plane[1],
+        m_sourceWidth, m_sourceHeight, im->stride[0], im->stride[1], m_sourceWidth * 4);
+      return;
+    }
+#endif
     m_sw_context = m_dllSwScale->sws_getCachedContext(m_sw_context,
       im->width, im->height, PIX_FMT_YUV420P,
       im->width, im->height, PIX_FMT_RGBA,
@@ -1352,7 +1357,6 @@ void CLinuxRendererGLES::UploadYV12Texture(int source)
     uint8_t *dst[]  = { m_rgbBuffer, 0, 0, 0 };
     int dstStride[] = { m_sourceWidth*4, 0, 0, 0 };
     m_dllSwScale->sws_scale(m_sw_context, src, srcStride, 0, im->height, dst, dstStride);
-#endif
   }
 
   bool deinterlacing;

xbmc/guilib/MatrixGLES.cpp
@@ -28,6 +28,9 @@
 #include <cmath>
 #include "MatrixGLES.h"
 #include "utils/log.h"
+#if defined(__ARM_NEON__)
+#include "utils/CPUInfo.h"
+#endif
 
 CMatrixGLES g_matrices;
 
@@ -43,6 +46,9 @@ CMatrixGLES::CMatrixGLES()
   }
   m_matrixMode = (EMATRIXMODE)-1;
   m_pMatrix    = NULL;
+#if defined(__ARM_NEON__)
+  m_has_neon = (g_cpuInfo.GetCPUFeatures() & CPU_FEATURE_NEON) == CPU_FEATURE_NEON;
+#endif
 }
 
 CMatrixGLES::~CMatrixGLES()
@@ -236,20 +242,19 @@ inline void Matrix4Mul(const float* src_mat_1, const float* src_mat_2, float* ds
     : "memory", "q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11" //clobber
     );
 }
+#endif
 void CMatrixGLES::MultMatrixf(const GLfloat *matrix)
 {
   if (m_pMatrix)
   {
-    GLfloat m[16];
-    Matrix4Mul(m_pMatrix, matrix, m);
-  }
-}
-
-#else
-void CMatrixGLES::MultMatrixf(const GLfloat *matrix)
-{
-  if (m_pMatrix)
-  {
+#if defined(__ARM_NEON__)
+    if (m_has_neon)
+    {
+      GLfloat m[16];
+      Matrix4Mul(m_pMatrix, matrix, m);
+      return;
+    }
+#endif
     GLfloat a = (matrix[0]  * m_pMatrix[0]) + (matrix[1]  * m_pMatrix[4]) + (matrix[2]  * m_pMatrix[8])  + (matrix[3]  * m_pMatrix[12]);
     GLfloat b = (matrix[0]  * m_pMatrix[1]) + (matrix[1]  * m_pMatrix[5]) + (matrix[2]  * m_pMatrix[9])  + (matrix[3]  * m_pMatrix[13]);
     GLfloat c = (matrix[0]  * m_pMatrix[2]) + (matrix[1]  * m_pMatrix[6]) + (matrix[2]  * m_pMatrix[10]) + (matrix[3]  * m_pMatrix[14]);
@@ -272,7 +277,6 @@ void CMatrixGLES::MultMatrixf(const GLfloat *matrix)
     m_pMatrix[3] = d;  m_pMatrix[7] = h;  m_pMatrix[11] = l;  m_pMatrix[15] = p;
   }
 }
-#endif
 
 // gluLookAt implementation taken from Mesa3D
 void CMatrixGLES::LookAt(GLfloat eyex, GLfloat eyey, GLfloat eyez, GLfloat centerx, GLfloat centery, GLfloat centerz, GLfloat upx, GLfloat upy, GLfloat upz)

xbmc/guilib/MatrixGLES.h
@@ -71,6 +71,9 @@ protected:
   std::vector<struct MatrixWrapper> m_matrices[(int)MM_MATRIXSIZE];
   GLfloat *m_pMatrix;
   EMATRIXMODE m_matrixMode;
+#if defined(__ARM_NEON__)
+  bool m_has_neon;
+#endif
 };
 
 extern CMatrixGLES g_matrices;

commit e2f67422ccc57bff9581ede3d08065c530b91d4b

[droid] fixed wrong logic introduced in e9ea5f7178e38b686c9d508c6fddccfadead151f

xbmc/cores/VideoRenderers/LinuxRendererGLES.cpp
@@ -1348,19 +1348,21 @@ void CLinuxRendererGLES::UploadYV12Texture(int source)
     {
       yuv420_2_rgb8888_neon(m_rgbBuffer, im->plane[0], im->plane[2], im->plane[1],
         m_sourceWidth, m_sourceHeight, im->stride[0], im->stride[1], m_sourceWidth * 4);
-      return;
     }
+    else
 #endif
-    m_sw_context = m_dllSwScale->sws_getCachedContext(m_sw_context,
-      im->width, im->height, PIX_FMT_YUV420P,
-      im->width, im->height, PIX_FMT_RGBA,
-      SWS_FAST_BILINEAR, NULL, NULL, NULL);
+    {
+      m_sw_context = m_dllSwScale->sws_getCachedContext(m_sw_context,
+        im->width, im->height, PIX_FMT_YUV420P,
+        im->width, im->height, PIX_FMT_RGBA,
+        SWS_FAST_BILINEAR, NULL, NULL, NULL);
 
-    uint8_t *src[]  = { im->plane[0], im->plane[1], im->plane[2], 0 };
-    int srcStride[] = { im->stride[0], im->stride[1], im->stride[2], 0 };
-    uint8_t *dst[]  = { m_rgbBuffer, 0, 0, 0 };
-    int dstStride[] = { m_sourceWidth*4, 0, 0, 0 };
-    m_dllSwScale->sws_scale(m_sw_context, src, srcStride, 0, im->height, dst, dstStride);
+      uint8_t *src[]  = { im->plane[0], im->plane[1], im->plane[2], 0 };
+      int srcStride[] = { im->stride[0], im->stride[1], im->stride[2], 0 };
+      uint8_t *dst[]  = { m_rgbBuffer, 0, 0, 0 };
+      int dstStride[] = { m_sourceWidth*4, 0, 0, 0 };
+      m_dllSwScale->sws_scale(m_sw_context, src, srcStride, 0, im->height, dst, dstStride);
+    }
   }
 
   bool deinterlacing;